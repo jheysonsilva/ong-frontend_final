5171990129be878a6a980add5be2e14a68090da1
Jheyson Silva Siqueira <jheyson0295@hotmail.com>
2025-10-26 21:20:02 -0300

    Refactor event listeners for dark mode and contrast

--- js/acessibilidade.js
+++ js/acessibilidade.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const body = document.body;
   const btnModoEscuro = document.getElementById('modoEscuroBtn');
   const btnAltoContraste = document.getElementById('altoContrasteBtn');
 
-  if (btnModoEscuro) btnModoEscuro.addEventListener('click', () => {
-    body.classList.toggle('modo-escuro');
+  btnModoEscuro?.addEventListener('click', () => {
+    document.body.classList.toggle('modo-escuro');
   });
 
-  if (btnAltoContraste) btnAltoContraste.addEventListener('click', () => {
-    body.classList.toggle('alto-contraste');
+  btnAltoContraste?.addEventListener('click', () => {
+    document.body.classList.toggle('alto-contraste');
   });
 });
